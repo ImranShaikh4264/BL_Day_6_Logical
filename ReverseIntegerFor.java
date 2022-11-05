b419ee5760ae3362387fd61a39606799dd426bb8
ImranShaikh4264 <shaikhimrana56@gmail.com>
2022-11-05 17:10:24 +0530

    Added:Program to reverse given integer value using for loop

--- ReverseIntegerFor.java
+++ ReverseIntegerFor.java
@@ -3,15 +3,23 @@ package com.bridgelab.day6.logical;
 import java.util.Scanner;
 
 public class ReverseIntegerFor {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter number");
-		int Integer = sc.nextInt(), reverse = 0;
+	public static int reverse(int number) {
+		int Integer = number;
+		int reverse = 0;
 		System.out.println("Given Integer is : " + Integer);
 		for (; Integer != 0; Integer = Integer / 10) {
 			int remainder = Integer % 10;
 			reverse = reverse * 10 + remainder;
 		}
+		return reverse;
+
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter number");
+		int Integer = sc.nextInt();
+		int reverse = reverse(Integer);
 		System.out.println("The reverse of the given Integer is: " + reverse);
 	}
 
